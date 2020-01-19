1711f4c1300d65befa43962f7cebeaca9cb3478e
pampas93 <abhijit.93@hotmail.com>
2020-01-19 03:03:51 -0700

    Spectator View test scene (Not working)

---
+++ Assets/Scripts/TestScripts/RotateObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+    public Vector3 Rotation = new Vector3(10.0f, 0.0f, 0.0f);
+    public float Speed = 5f;
+
+    void Update()
+    {
+        transform.Rotate(Rotation * Time.deltaTime * Speed);
+    }
+}
