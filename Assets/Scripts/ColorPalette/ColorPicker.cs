9bd07fe112cb17f2fd4ac473e59f2732966aafed
pampas93 <abhijit.93@hotmail.com>
2020-01-19 14:53:34 -0700

    * Working Color Palette scene (Palette scene)

---
+++ Assets/Scripts/ColorPalette/ColorPicker.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorPicker : MonoBehaviour
+{
+    public Renderer QuadRenderer;
+    public SliderIndicator Slider;
+    public Image PickedColor;
+
+    Texture2D texture;
+
+    void Start()
+    {
+        texture = QuadRenderer.material.mainTexture as Texture2D;
+        Slider.OnCollision += OnIndicatorChanged;
+    }
+
+    void OnIndicatorChanged(Vector3 contactPoint)
+    {
+        if (Physics.Raycast(contactPoint, Vector3.forward, out RaycastHit oHit, 100f))
+        {
+            var rend = oHit.collider.GetComponent<Renderer>();
+            if (rend == null)
+                return;
+            var pixelUV = oHit.lightmapCoord;
+            var color = texture.GetPixelBilinear(pixelUV.x, pixelUV.y);
+            PickedColor.color = color;
+        }
+
+    }
+}
---
+++ Assets/Scripts/ColorPalette/SliderIndicator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+public class SliderIndicator : MonoBehaviour
+{
+    public GameObject OtherIndicator;
+    public Action<Vector3> OnCollision;
+
+    void OnCollisionStay(Collision colliderObj)
+    {
+        if (colliderObj.gameObject != OtherIndicator) return;
+        // Debug.Log("First point that collided: " + other.contacts[0].point);
+        OnCollision?.Invoke(colliderObj.contacts[0].point);
+    }
+}
